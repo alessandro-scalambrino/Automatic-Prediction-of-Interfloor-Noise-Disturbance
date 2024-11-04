c0a942405507e1c80b61f56b1c49feadebd57359
alessandro-scalambrino <156260456+alessandro-scalambrino@users.noreply.github.com>
2024-11-04 12:47:39 +0100

    Update main.py, added dictionaries for cleaner main

--- main.py
+++ main.py
@@ -4,36 +4,74 @@ from functions.func_stat import *
 from functions.func_machine_learning import *
 from functions.func_plot import *
 
-
-
-
+# Load data
 excel_path = './elenco_file_audio_backup.xlsx'
 df = load_excel_data(excel_path)
 audio_folder = './dataset_rand'
 soggetti = ['soggetto1', 'soggetto2', 'soggetto3', 'soggetto4', 'soggetto5', 'soggetto6']
 
-#   ANALISI STATISTICA DEI RISULTATI
-# Calcola e stampa le statistiche
-media_per_soggetto, deviazione_standard_per_soggetto, mediana_per_soggetto, intervallo_per_soggetto, kappa_scores = calculate_patient_statistics(df, soggetti)
-df = calculate_sound_statistics(df, soggetti)
-media_per_categoria, devstd_per_categoria, mediana_per_categoria, intervallo_per_categoria, categorie = calculate_category_statistics(df, soggetti)
+# Initialize a results dictionary for statistical analysis
+statistics_results = {
+    'patient_statistics': {},
+    'sound_statistics': {},
+    'category_statistics': {}
+}
 
-print_patient_statistics(soggetti, media_per_soggetto, deviazione_standard_per_soggetto, mediana_per_soggetto, intervallo_per_soggetto)
-print_sound_statistics(df)
-print_category_statistics(media_per_categoria, devstd_per_categoria, mediana_per_categoria, intervallo_per_categoria, categorie, soggetti)
+# Calculate patient statistics
+statistics_results['patient_statistics']['media'], statistics_results['patient_statistics']['deviazione_standard'], \
+statistics_results['patient_statistics']['mediana'], statistics_results['patient_statistics']['intervallo'], \
+statistics_results['patient_statistics']['kappa_scores'] = calculate_patient_statistics(df, soggetti)
 
-# k-statistics
-for coppia, kappa_score in kappa_scores.items():
+# Calculate sound statistics
+statistics_results['sound_statistics'] = calculate_sound_statistics(df, soggetti)
+
+# Calculate category statistics
+statistics_results['category_statistics']['media'], statistics_results['category_statistics']['devstd'], \
+statistics_results['category_statistics']['mediana'], statistics_results['category_statistics']['intervallo'], \
+statistics_results['category_statistics']['categorie'] = calculate_category_statistics(df, soggetti)
+
+# Centralized printing function
+def print_statistics(statistics, soggetti):
+    print("Patient Statistics:")
+    print_patient_statistics(
+        soggetti,
+        statistics['patient_statistics']['media'],
+        statistics['patient_statistics']['deviazione_standard'],
+        statistics['patient_statistics']['mediana'],
+        statistics['patient_statistics']['intervallo']
+    )
+
+    print("\nSound Statistics:")
+    print_sound_statistics(statistics['sound_statistics'])
+
+    print("\nCategory Statistics:")
+    print_category_statistics(
+        statistics['category_statistics']['media'],
+        statistics['category_statistics']['devstd'],
+        statistics['category_statistics']['mediana'],
+        statistics['category_statistics']['intervallo'],
+        statistics['category_statistics']['categorie'],
+        soggetti
+    )
+
+# Print all statistics
+print_statistics(statistics_results, soggetti)
+
+# Kappa statistics output
+for coppia, kappa_score in statistics_results['patient_statistics']['kappa_scores'].items():
     print(f"Cohen's Kappa tra {coppia[0]} e {coppia[1]}: {kappa_score}")
 
+# Prepare data for machine learning
 df_excel = pd.read_excel(excel_path)
 df = calculate_average_ratings(df_excel, excel_path, audio_folder)
-# chiamata a prepare data (estrazione feature e creazione dei set di train/test)
+
+# Call prepare_data function to extract features and create train/test sets
 result = prepare_data(df, audio_folder)
+
 print("Dataframe:")
 print(result)
+
 if result is not None:
- 
     X_train, X_test, y_train, y_test, F_train, F_test, z_train, z_test = result
     print("---X_train----------------")
     print(X_train)
@@ -51,19 +89,33 @@ if result is not None:
     print(z_train)
     print("---z_test----------------")
     print(z_test)
-    
-# MACHINE LEARNING
-    linear_pred, linearf_pred, mse_linear, mae_linear = linear_regression(X_train, X_test, z_train, z_test, F_train, F_test)
-    knn_pred, knnf_pred, _, knn_accuracy, knnf_accuracy, mse, mae = knn_classification(X_train, X_test, y_train, y_test, F_train, F_test, z_train, z_test)
-    tree_pred, treef_pred, tree_accuracy, treef_accuracy, tree_reg_mse, tree_reg_mae = decision_tree_classification(X_train, X_test, y_train, y_test, F_train, F_test, z_train, z_test)
 
-    svm_pred, svmf_pred, svm_accuracy, svmf_accuracy, svm_linear_pred, svmf_linear_pred, svm_linear_accuracy, svmf_linear_accuracy, svm_poly_pred, svmf_poly_pred, svm_poly_accuracy, svmf_poly_accuracy, svm_rbf_pred, svmf_rbf_pred, svm_rbf_accuracy, svmf_rbf_accuracy, svr_linear_mse, svr_linear_mae, svr_poly_mse, svr_poly_mae, svr_rbf_mse, svr_rbf_mae = svm_classification(X_train, X_test, y_train, y_test, F_train, F_test, z_train, z_test)
-#plot dei risultati
-    plot_results(z_test, linear_pred, linearf_pred, y_test, knn_pred, knn_accuracy, knnf_pred, knnf_accuracy,
-                 tree_pred, tree_accuracy, treef_pred, treef_accuracy, svm_pred, svm_accuracy, svmf_pred,
-                 svmf_accuracy, svm_linear_pred, svmf_linear_pred, svm_linear_accuracy, svmf_linear_accuracy,
-                 svm_poly_pred, svmf_poly_pred, svm_poly_accuracy, svmf_poly_accuracy, svm_rbf_pred, svmf_rbf_pred,
-                 svm_rbf_accuracy, svmf_rbf_accuracy, mse_linear, mae_linear, mse, mae, tree_reg_mse, tree_reg_mae, svr_linear_mse, svr_linear_mae, svr_poly_mse, svr_poly_mae, svr_rbf_mse, svr_rbf_mae)
+    # MACHINE LEARNING
+    predictions = {}
+
+    # Linear Regression
+    predictions['linear'] = linear_regression(X_train, X_test, z_train, z_test, F_train, F_test)
+    
+    # KNN Classification
+    predictions['knn'] = knn_classification(X_train, X_test, y_train, y_test, F_train, F_test, z_train, z_test)
+
+    # Decision Tree Classification
+    predictions['tree'] = decision_tree_classification(X_train, X_test, y_train, y_test, F_train, F_test, z_train, z_test)
+
+    # SVM Classification
+    predictions['svm'] = svm_classification(X_train, X_test, y_train, y_test, F_train, F_test, z_train, z_test)
+
+    # Plot the results
+    plot_results(
+        z_test,
+        predictions['linear'][0], predictions['linear'][1], predictions['linear'][2], predictions['linear'][3],
+        predictions['knn'][0], predictions['knn'][1], predictions['knn'][2], predictions['knn'][3],
+        predictions['tree'][0], predictions['tree'][1], predictions['tree'][2], predictions['tree'][3],
+        predictions['svm'][0], predictions['svm'][1], predictions['svm'][2], predictions['svm'][3],
+        predictions['svm'][4], predictions['svm'][5], predictions['svm'][6], predictions['svm'][7],
+        predictions['svm'][8], predictions['svm'][9], predictions['svm'][10], predictions['svm'][11],
+        predictions['svm'][12], predictions['svm'][13], predictions['svm'][14], predictions['svm'][15]
+    )
 
 else:
     print("Dataframe vuoto")
